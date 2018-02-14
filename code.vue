696914c39bd6c0bbf93e33f2103825200395629d
shavleg <shavlegkakulia@gmail.com>
2018-02-14 18:57:19 +0400

    Update code.vue

--- code.vue
+++ code.vue
@@ -1,223 +1,160 @@
 <template>
-  <div class="ui fluid card" ref="comment">
-    <div class="content">
-      <!-- Avatar -->
+  <div class="eleven wide computer sixteen wide mobile centered column">
+    <div class="ui items">
+      <div class="item">
+        <!-- Avatar -->
+        <!-- <div class="ui small circular image">
+          <img :src="'https://avtorebi.com/img/ZsjhAUfRpalj9ncI.jpg?w=192&h=192&fit=crop'">
+           <a href="#"><span class="edit icon"></span></a>
+        </div> -->
 
-      <!-- Name -->
-      <router-link :to="{ name: 'author', params: { id: comment.author.id } }" class="header fw-normal" style="font-size: 15px; margin-top: 0;">
-        {{ comment.author.name }}
-      </router-link>
-      <!-- Date -->
-      <div class="meta">
-        <span v-timeago="comment.created_at"></span>
-      </div>
-      <!-- Content -->
-      <div class="description postComment">
-        <div v-if="editMode" class="postComment--editor">
-          <quill-editor
-            v-model="editedComment"
-            :config="editorConfig"
-          ></quill-editor>
+                <!-- Avatar -->
+        <div ref="cover"
+          class="ui small circular image"
+
+        >
+          <!-- <div class="ui dimmer">
+            <div class="content">
+              <div class="center">
+                <div @click="$refs.avatarSelect.click()" class="ui small purple button kaps fw-bold">
+                  შეცვლა
+                </div>
+              </div>
+            </div>
+          </div> -->
+          <img :src="'https://avtorebi.com/img/ZsjhAUfRpalj9ncI.jpg?w=192&h=192&fit=crop'">
+          <div class="overlay"></div>
+  <div class="button"><a @click="$refs.coverSelect.click()" > შეცვლა </a></div>
         </div>
+        <input ref="coverSelect" type="file" accept="image/*" style="display: none;">
 
-        <div v-else v-html="comment.content" class="postComment--content"></div>
-      </div>
-    </div>
+        <div class="content" style="margin: auto; text-align: ლეფტ;">
+          <!-- Name -->
+          <div class="ui large header" >
+            {{ tags.list[0].name }}
+          </div>
 
-    <!-- Extra -->
-    <div class="extra content">
-      <div class="left floated">
-        <div v-show="!editMode">
-            <!-- Upvote -->
-            <div @click="$click_vote"
-                 :class="{ blue: comment.user_has.voted }"
-                 class="tiny ui basic icon button kaps"
-            >
-                <i :class="{ remove: comment.user_has.voted, 'thumbs outline up': !comment.user_has.voted }"
-                   class="icon"
-                ></i> ხმის მიცემა {{ comment.total.votes }}
-            </div>
+          <!-- Follow -->
+          <follow-button
+            v-if="isMe"
+            :user="posts"
+            :styles='{ "margin-left": "10px", "vertical-align": "super" }'
+            @toggle="true"
+          ></follow-button>
 
-            <!-- Edit -->
-            <div v-if="isOwner"
-                 @click="$click_edit"
-                 class="tiny ui basic icon button kaps"
-            >
-                <i class="write icon"></i> რედაქტირება
-            </div>
+          <!-- About -->
+          <div class="meta">
+            <span>{{ tags.list[0].slug }}</span>
+          </div>
 
-            <!-- Delete -->
-            <div v-if="isOwner"
-                 @click="$click_delete"
-                 class="tiny ui basic icon button kaps"
-            >
-                <i class="remove icon"></i> წაშლა
-            </div>
-
-            <!-- Comments -->
-            <div
-                 @click="$click_comment_toogle"
-                 class="tiny ui basic icon button kaps"
-            >
-                <i class="commenting icon"></i> კომენტარები
-            </div>
-        </div>
-        <div v-show="editMode">
-            <!-- Save -->
-            <div @click="$click_save"
-                 :class="{ loading: isSaving }"
-                 class="tiny ui basic icon button kaps"
-            >
-                <i class="save icon"></i> შენახვა
-            </div>
-
-            <!-- Cancel -->
-            <div @click="$click_cancel"
-                 class="tiny ui basic icon button kaps"
-            >
-                <i class="remove icon"></i> გაუქმება
-            </div>
+          <!-- Author Types -->
+          <!-- <div class="description">
+            <span v-for="type in tags">
+              {{ type.name }} &#9651;
+            </span>
+          </div> -->
         </div>
       </div>
-    </div>
-    <div class="social_comments" v-bind:id="'social_comments_'+comment.id">
-
-      <div class="fb-comments"   v-bind:data-href="href_data+'/'+comment.id" data-numposts="5"></div>
-
     </div>
   </div>
 </template>
 
 <script>
-  import { quillEditor as QuillEditor } from 'vue-quill-editor'
-  import { VoteService, CommentService } from '../../services'
-  import { editorConfig } from './exports'
-  import _ from 'underscore'
+import FollowButton from "../../components/FollowButton.vue";
 
-  export default {
-    components: { QuillEditor },
+export default {
+  components: { FollowButton },
 
-    props: {
-      comment: { type: Object, required: true },
-      user: { type: Object, required: true },
-      scroll: { type: Boolean, default: false }
+  props: {
+    posts: {
+      type: Object,
+      required: true
     },
-
-    data () {
-      return {
-        ok:false,
-        isSaving: false,
-        editMode: false,
-        editorConfig: editorConfig,
-        editedComment: '',
-        href_data:document.location.href
-      }
-    },
-
-    computed: {
-      'isOwner': function () {
-        return this.comment.author.id === this.user.id
-      }
-    },
-
-    methods: {
-      $click_vote () {
-        this.comment.user_has.voted = !this.comment.user_has.voted
-
-        VoteService.comment(this.comment.id, this.comment.user_has.voted)
-          .then(() => {
-            this.comment.total.votes += this.comment.user_has.voted ? 1 : -1
-          }, () => {
-            this.comment.user_has.voted = !this.comment.user_has.voted
-          })
-      },
-
-      $click_edit () {
-        this.editedComment = this.comment.content
-        this.editMode = true
-      },
-
-      $click_comment_toogle()
-      {
-        var el = '#social_comments_'+this.comment.id;
-        var clicks = $(this).data('clicks');
-        if (!clicks) {
-          $(el).animate({height:'300px'},500, function(){$(el).css('overflow','auto');});
-
-        } else {
-          $(el).animate({height:'0'},500, function(){$(el).css('overflow','hidden');});
-        }
-        $(this).data("clicks", !clicks);
-      },
-
-      $click_cancel () {
-        this.editMode = false
-        this.editedComment = '';
-      },
-
-      $click_save () {
-        if (_.isEmpty(this.editedComment)) {
-          return
-        }
-
-        this.isSaving = true
-        CommentService.update(this.comment.id, this.editedComment)
-          .then(response => {
-            this.$emit('update', response.data)
-          })
-          .finally(() => {
-            this.isSaving = false
-            this.$click_cancel()
-          })
-      },
-
-      $click_delete () {
-        this.$bus.$emit('modal:confirm', this.$lang.comment.deleteMessage, response => {
-          if (response === 'ok') {
-            CommentService.delete(this.comment.id)
-              .then(() => {
-                this.$emit('delete', this.comment)
-              })
-          }
-        })
-      }
-    },
-
-    mounted () {
-
-
-
-      //$('.fb-comments').attr('data-href',document.location.href);
-      // Scroll to comment
-      if (this.scroll) {
-        setTimeout(() => {
-          $('html, body').animate({
-            scrollTop: $(this.$refs.comment).offset().top
-          }, 500)
-        }, 500)
-      }
-
-            this.$nextTick(function () {
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/ka_GE/sdk.js#xfbml=1&version=v2.12';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'))
-  })
+    tags: {
+      type: Object,
+      required: true
     }
+  },
+
+  computed: {
+    isMe() {
+      return true; /*this.$store.state.user.id === this.tags.id*/
+    }
+  },
+  methods: {
+    updateCover(avatar) {
+      this.$progress.start();
+      //UserService.updateAvatar(this.user.id, avatar)
+      UserService.updateAvatar(1, avatar)
+        .then(() => this.fetchUser())
+        .finally(() => this.$progress.done());
+    }
+  },
+  mounted() {
+    this.$progress.done();
+    this.$nextTick(() => {
+      $(this.$refs.coverSelect).on("change", evt => {
+        alert();
+        this.updateCover(evt.target.files[0]);
+      });
+
+      // this.setSelectedAuthorTypes(this.user.author_types)
+    });
   }
+};
 </script>
 
 <style>
-.postComment--editor .ql-toolbar.ql-snow,
-.postComment--editor .ql-container.ql-snow {
-  border: none !important;
+.ui.small.image {
+  position: relative;
+  /* margin-top: 50px;*/
+  width: 100px;
+  height: 100px;
 }
-.social_comments
-{
-  height: 0px;
-  overflow: hidden;
+
+.ui.small.image .overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0);
+  transition: background 0.5s ease;
+}
+
+.ui.small.image:hover .overlay {
+  display: block;
+  background: rgba(0, 0, 0, 0.3);
+}
+
+.ui.small.image img {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  left: 0;
+}
+
+.ui.small.image .button {
+  position: absolute;
+  width: 100px;
+  left: 0;
+  top: 40px;
+  text-align: center;
+  opacity: 0;
+  transition: opacity 0.35s ease;
+}
+
+.ui.small.image .button a {
+  /* width: 200px; */
+  padding: 12px 4px;
+  text-align: center;
+  color: white;
+  border: solid 2px white;
+  z-index: 1;
+  background-color: #a333c8;
+}
+
+.ui.small.image:hover .button {
+  opacity: 1;
 }
 </style>
